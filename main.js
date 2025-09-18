31d85b73ea29a49df17d519288bb117ec3724631
NikoShoo <NikoWork1@ya.ru>
2025-09-18 15:43:49 +0400

    Находит элементы внутри узла DOM, соответсвующий селектору .todo

---
+++ main.js
@@ -0,0 +1,8 @@
+const toDo = document.querySelector('.todo');
+const todoInput = toDo.querySelector('.todo__input');
+const todoBtn = toDo.querySelector('.todo__btn');
+const todoList = toDo.querySelector('.todo__btn');
+
+
+
+
